abbf3178d6775c878282ae38fa2e636155460c13
Spencer Davis <spencerdavis226@gmail.com>
2025-03-10 13:47:07 -0400

    Added GET route for specific books

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { connectToDb, getDb } = require('./db');
+const { ObjectId } = require('mongodb');
 
 // init app & middleware
 const app = express();
@@ -18,5 +19,34 @@ connectToDb((err) => {
 
 // routes
 app.get('/books', (req, res) => {
-  res.json({ msg: 'Welcome to the API' });
+  let books = [];
+  db.collection('books')
+    .find()
+    .sort({ author: 1 })
+    .forEach((book) => books.push(book))
+    .then(() => {
+      res.status(200).json(books);
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'could not fetch the documents' });
+    });
+});
+
+app.get('/books/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection('books')
+      .findOne({ _id: new ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: 'Could not fetch the document' });
+      });
+  } else {
+    res.status(500).json({ error: 'Not a valid document ID' });
+  }
+});
+
+app.post('/books', (req, res) => {
+  
 });
