981fee83a68fcb19b95b3699f311e7e50b7fd259
Spencer Davis <spencerdavis226@gmail.com>
2025-03-10 12:43:17 -0400

    Set up app.js starter code and installed dependencies

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express');
 // init app & middleware
 const app = express();
 
+// routes
+app.get('/books', (req, res) => {
+  res.json({ msg: 'Welcome to the API' });
+});
+
 app.listen(3000, () => {
   console.log('App listening on port 3000');
 });
